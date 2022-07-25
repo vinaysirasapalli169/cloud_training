ad4de4a0d0eac91e41c22663e15c491f778c9cd3
vinaysirasapalli169 <vinaysirasapall@tigeranalytics.com>
2022-07-25 11:51:00 +0530

    added_files

---
+++ hello.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os, uuid
+from flask import jsonify
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    name = '1'
+    print(name)
+    res = []
+    connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    print(connect_str)
+    blob_service_client = ContainerClient.from_connection_string(connect_str,container_name="data")
+    blob_list = blob_service_client.list_blobs()
+    print(blob_service_client)
+    print(blob_list)
+    for blob in blob_list:
+        temp = str(blob.name)
+        folder,file = temp.split('/')
+        print(folder,name)
+        if folder  ==  name:
+            res.append(file)
+    return jsonify(res)
+    
